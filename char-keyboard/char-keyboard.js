97ede90f0f60e5d17c1bc87e068940319aff1c23
henrym2 <henrym2@tcd.ie>
2019-05-02 00:25:13 +0100

    Created keyboard ascii simulation

--- char-keyboard/char-keyboard.js
+++ char-keyboard/char-keyboard.js
@@ -1,8 +1,26 @@
+var keyboardInitial = (_=>[..."`1234567890-=~~QWERTYUIOP[]\\~ASDFGHJKL;'~~ZXCVBNM,./~"].map(x=>(o+=`/${b='_'.repeat(w=x<y?2:' 667699'[x=["BS","TAB","CAPS","ENTER"][p++]||'SHIFT',p])}\\|`,m+=y+(x+'    ').slice(0,w)+y+y,n+=y+b+y+y,l+=' __'+b)[73]&&(k.push(l,m,n,o),l='',m=n=o=y),m=n=o=y='|',p=l=k=[])&&k.join`
+`)();
+var keyboardPrint = keyboardInitial;
+
 function setup() {
-    
+ 
+    createCanvas(600, 500)
+    background(0)
 }
 
 function draw() {
+    
+    background(0);
+    fill(255,255,255);
+    textFont('monospace')
+    text(keyboardPrint, 0, 0, 600, 500)
+    keyboardPrint = keyboardInitial;
+    
+}
 
+function keyTyped(){
+    console.log(key.toUpperCase())
+    console.log(keyboardInitial);
+    keyboardPrint = keyboardPrint.replace(key.toUpperCase(), " ");
 }
 
